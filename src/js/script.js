fc5a7b00fce1e3f85af72af8f397763c4f8a647e
Zzzzz <vyacheslav17@mail.ru>
2020-06-29 18:08:02 +0300

    The task how to fix scripts!

--- src/js/script.js
+++ src/js/script.js
@@ -1,53 +1,54 @@
-let tgSwitch = document.getElementById('tgSwitch')
-let slider = document.getElementById('slider')
-let tgSwitchInput = document.getElementById('tgSwitchInput')
+(function(){
+
+let tgSwitch = document.getElementById('tgSwitch');
+let slider = document.getElementById('slider');
+let tgSwitchInput = document.getElementById('tgSwitchInput');
+
+
+/* tgSwitch и tgSwitch2 объединить в одну функцию */
 tgSwitch.onclick = () => {
     if (tgSwitch.className === 'tgSwitch-practicing-days') {
-        tgSwitch.className += ' active'
-        slider.style.transform = 'translateX(30px)'
-        slider.style.borderColor = 'green'
+        tgSwitch.className += ' active';  /* строки 11-13 Добавить стиль "active" и менять по команде classlist.toggle  */
+        slider.style.transform = 'translateX(30px)';
+        slider.style.borderColor = 'green';
         tgSwitchInput.setAttribute('checked', 'checked')
     } else {
-        tgSwitch.className = 'tgSwitch-practicing-days'
-        slider.style.transform = 'translateX(0)'
-        slider.style.borderColor = '#C4C9CB'
+        tgSwitch.className = 'tgSwitch-practicing-days';
+        slider.style.transform = 'translateX(0)';
+        slider.style.borderColor = '#C4C9CB';
         tgSwitchInput.removeAttribute('checked')
     }
-}
+};
 
-let tgSwitch2 = document.getElementById('tgSwitch2')
-let slider2 = document.getElementById('slider2')
-let tgSwitchInput2 = document.getElementById('tgSwitchInput2')
+let tgSwitch2 = document.getElementById('tgSwitch2');
+let slider2 = document.getElementById('slider2');
+let tgSwitchInput2 = document.getElementById('tgSwitchInput2');
 tgSwitch2.onclick = () => {
     if (tgSwitch2.className === 'tgSwitch-practicing-days') {
-        tgSwitch2.className += ' active'
-        slider2.style.transform = 'translateX(30px)'
-        slider2.style.borderColor = 'green'
+        tgSwitch2.className += ' active';
+        slider2.style.transform = 'translateX(30px)';
+        slider2.style.borderColor = 'green';
         tgSwitchInput2.setAttribute('checked', 'checked')
     } else {
-        tgSwitch2.className = 'tgSwitch-practicing-days'
-        slider2.style.transform = 'translateX(0)'
-        slider2.style.borderColor = '#C4C9CB'
+        tgSwitch2.className = 'tgSwitch-practicing-days';
+        slider2.style.transform = 'translateX(0)';
+        slider2.style.borderColor = '#C4C9CB';
         tgSwitchInput2.removeAttribute('checked')
     }
 };
 
 
 
-
-/* IIFE - самовызывающаяся функция. Скрывает переменные из глобальной области видимости. */
-
-(function(){
     let allFrontDays = document.getElementsByClassName('front-week-day');
     for (let elem of allFrontDays){
     elem.addEventListener('click', toggleDay)
     }
     function toggleDay() {
         if (this.className === 'front-week-day') {
-            this.className += ' active-week-days'
+            this.className += ' active-week-days';
             this.nextElementSibling.setAttribute('checked', 'checked')
         } else {
-            this.className = 'front-week-day'
+            this.className = 'front-week-day';
             this.nextElementSibling.removeAttribute('checked')
         }
     }
